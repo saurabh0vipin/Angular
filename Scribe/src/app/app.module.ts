d40294469627b41a70733b7742793e912377cbb0
saurabh0vipin <saurabhvipin55@gmail.com>
2020-04-18 02:57:48 +0530

    NgModel1

--- Scribe/src/app/app.module.ts
+++ Scribe/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { Module2Component } from './module2/module2.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +14,7 @@ import { Module2Component } from './module2/module2.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
